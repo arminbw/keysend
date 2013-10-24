07a7c1a4f59c45e3ea5d673381d92e88d739b85f
pi <pi@brokenrul.es>
2013-10-24 15:04:17 +0200

    Added argparse parsing

--- keysend.py
+++ keysend.py
@@ -1,58 +1,92 @@
 #!/usr/bin/env python3
 
-import sys, argparse, smtplib
+import sys, argparse, smtplib, time, csv, os
 from email.mime.text import MIMEText
 
-smtp_user = 'johndoe1972@example.com'      # smtp user name for authentification
-smtp_password = '12345'                    # smtp password for authentification
-smtp_server = 'smtp.example.com'           # smtp server address
-from_address = 'myOwnAddress@example.com'  # email address of the sender (that's you!)
-subject = 'Your personal key'              # subject of the email
-addresses_filename = 'addresses.txt'       # file with one email address per line
-keys_filename = 'keys.txt'                 # file with one key per line
-template_filename = 'template.txt'         # template file for the email
+
+
+
+
+parser = argparse.ArgumentParser(description='Send keys in a file to eMail addresses in another file.')
+parser.add_argument('--smtp_user', '-u', help='SMTP username')
+parser.add_argument('--smtp_password', '-p', nargs=1, help='SMTP password')
+parser.add_argument('--smtp_server', '-e', nargs=1, help='SMTP server')
+parser.add_argument('--from_address', '-f', nargs=1, help='From address')
+parser.add_argument('--subject', '-s', nargs=1, help='Subject')
+parser.add_argument('--addresses_filename', '-a', nargs=1, type=argparse.FileType('r'), help='File containing lines with <name> TAB <eMail address>')
+parser.add_argument('--keys_filename', '-k', nargs=1, type=argparse.FileType('r'), help='File containing keys. One key per line.')
+parser.add_argument('--template_filename', '-t', nargs=1, help='File containing eMail body template. [FIRST_NAME] is replaced with first name, [NAME] with full name and [KEY] is replaced with key')
+parser.add_argument('--bcc_address', '-b', nargs='?', help='Bcc address')
+parser.add_argument('--dryrun', '-d', help='Dry run. Do not send anything', action="store_true")
+
+class Args(object):
+	pass
+
+args = parser.parse_args()
+print vars(args)
+
+dryrun = False
+if args.dryrun:
+	dryrun = True
+
+bcc = False
+if args.bcc_address is not None:
+	bcc = True
 
 def send_email(from_address, to_address, text, subject):
-    message = MIMEText(text)
-    message['From'] = from_address
-    message['To'] = to_address
-    message['Subject'] = subject
-    try:
-        server.sendmail(from_address, to_address, message.as_string())
-        print('mail sent successfully.\n')
-    except:
-        print('failed to send mail.\n')
-        server.close()
-        raise
+	message = MIMEText(text)
+	message['From'] = args.from_address
+	message['To'] = args.to_address
+	message['Subject'] = args.subject
+	if bcc:
+		message['Bcc'] = args.bcc_address
+	try:
+		if dryrun:
+			print message.as_string()
+		else:
+			# server.sendmail(from_address, to_address, message.as_string())
+			print "sendmail"
+		print('mail sent successfully.\n')
+	except:
+		print('failed to send mail.\n')
+		server.close()
+		raise
 
 def get_file_content(filename):
-    with open(filename, 'r') as fp:
-        content = fp.read()
-    return content
+	with open(filename, 'r') as fp:
+		content = fp.read()
+	return content
 
-addresses = get_file_content(addresses_filename).splitlines()
-keys = get_file_content(keys_filename).splitlines()
+addresses = get_file_content(args.addresses_filename).splitlines()
+keys = get_file_content(args.keys_filename).splitlines()
 if len(addresses) > len(keys):
-    print('not enough keys provided (%i addresses, only %i keys)' % (len(addresses), len(keys)))
-    sys.exit(1)
-template = get_file_content(template_filename)
+	print('not enough keys provided (%i addresses, only %i keys)' % (len(addresses), len(keys)))
+	sys.exit(1)
+template = get_file_content(args.template_filename)
 
 try:
-    server = smtplib.SMTP(smtp_server, 587)
-    server.ehlo()
-    server.starttls()
-    server.login(smtp_user, smtp_password)
+	server = smtplib.SMTP(args.smtp_server, 587)
+	server.ehlo()
+	server.starttls()
+	server.login(args.smtp_user, args.smtp_password)
 except smtplib.SMTPAuthenticationError:
-    print('failed to connect to smtp server. Username or password wrong.')
-    sys.exit(1)
+	print('failed to connect to smtp server. Username or password wrong.')
+	sys.exit(1)
 except:
-    print('failed to connect to smtp server.')
-    raise
+	print('failed to connect to smtp server.')
+	raise
 
 keyNumber = 0
-for address in addresses:
-    text = template.replace("PERSONALKEY", keys[keyNumber])    
-    print("email no.%i    address: %s    key: %s" % (keyNumber+1, address, keys[keyNumber]))
-    send_email(from_address, address, text, subject)
-    keyNumber += 1
+for line in csv.reader(addresses, delimiter="\t"):
+	text = template.replace("[KEY]", keys[keyNumber])
+
+	name = line[0].split()[0]
+
+	text = text.replace("[NAME]", name)
+	print("-- %i	To: %s <%s> Key: %s --" % (keyNumber, name, line[1], keys[keyNumber]))
+	send_email(args.from_address, line[1], text, args.subject)
+
+	time.sleep(0.5)
+	keyNumber += 1
+	
 server.close()
